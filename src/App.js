ff921e407e3ad342ae3e7451b8cb026143354eb2
Bhavesh-Gihar <97470501+Bhavesh-Gihar@users.noreply.github.com>
2023-03-09 18:52:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './teamPage.css'
+import TeamMembers from './components/TeamMembers.js'
+
+function App() {
+  return (
+    <div className="App">
+      <TeamMembers />
+    </div>
+  );
+}
+
+export default App;
